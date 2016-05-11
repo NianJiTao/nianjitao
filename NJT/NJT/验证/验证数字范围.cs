968e35ed192ef1e21759ecaba63d69fd4e955748
nianjitao <nianjitao@NIANOFFICE>
2016-05-11 13:59:14 +0800

    更新2016-5-11

---
+++ NJT/NJT/验证/验证数字范围.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NJT.验证
+{
+    public class 验证数字范围 : ValidationRule
+    {
+        private int min;
+
+        public int Min
+        {
+            get { return min; }
+            set { min = value; }
+        }
+
+        private int max;
+
+        public int Max
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            int number;
+            if (!int.TryParse((string)value, out number))
+            {
+                return new ValidationResult(false, "请输入数字");
+            }
+
+            if (number < min || number > max)
+            {
+                return new ValidationResult(false, string.Format("请在此范围内输入:{0},{1}", min, max));
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
