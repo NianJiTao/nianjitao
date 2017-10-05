167168f1ab349ab7be20219a245c679bdc57a59e
年纪涛 <NianJiTao@outlook.com>
2017-10-05 10:03:21 +0800

    升级版本nlog

--- NJT/NJT.Core/I按钮显示.cs
+++ NJT/NJT.Core/I按钮显示.cs
@@ -11,21 +11,6 @@ namespace NJT.Core
     {
     }
 
-    public static partial class 扩展
-    {
-        public static void 更新显示(this I按钮显示 按钮表, string[] 隐藏按钮字串)
-        {
-            var t = 按钮表.GetType();
-            var b = typeof(bool);
-            var properties = t.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
-            var sel = properties
-                .Where(x => x.PropertyType == b && x.CanWrite)
-                .ToList();
-            foreach (var item in sel)
-            {
-                item.SetValue(按钮表, 隐藏按钮字串.Contains(item.Name) == false);
-            }
-        }
-    }
+   
 
 }
---
+++ NJT/NJT.Ext/Type扩展.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJT.Ext
+{
+    public static partial class 扩展方法
+    {
+
+        public static IList<object> Get静态属性(this Type typex)
+        {
+            var r = typex.GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(x => x.GetValue(null)).ToList();
+            return r;
+        }
+
+        public static IList<object> Get静态字段(this Type typex)
+        {
+            var r = typex.GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Select(x => x.GetValue(null)).ToList();
+            return r;
+        }
+    }
+}
