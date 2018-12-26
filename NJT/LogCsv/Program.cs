4649c4c6b646592a16c61df1e1bf08178f689f98
年纪涛 <NianJiTao@outlook.com>
2018-12-26 20:19:59 +0800

    logcsv

---
+++ NJT/LogCsv/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace LogCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    Console.WriteLine($" 设备： {o.设备名称.First()}");
+                    Console.WriteLine($" 文件： {o.文件名称}");
+                    Console.WriteLine($" 内容： {o.内容}");
+                    Console.ReadLine();
+                });
+        }
+    }
+}
