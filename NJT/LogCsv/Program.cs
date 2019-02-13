78173ecdcbff6d6b730c9334c28f364bcafcbf8b
年纪涛 <NianJiTao@outlook.com>
2019-02-13 15:02:21 +0800

    del

--- NJT/LogCsv/Program.cs
+++ NJT/LogCsv/Program.cs
@@ -13,6 +13,10 @@ namespace LogCsv
         {
             try
             {
+                if (args.Length==0)
+                {
+                    //logcsv.exe - f f1 - n abc - c 时间,温度1,温度2
+                }
                 Parser.Default.ParseArguments<Csv内容>(args)
                     .WithParsed(x => { new CsvRw().写入(x, Properties.Settings.Default.输出目录); });
             }
